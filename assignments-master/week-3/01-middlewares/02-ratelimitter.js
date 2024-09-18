1c502107910ce0b960a107a72a81c584e6bda344
viditsri184 <viditsrivastava760@gmail.com>
2024-09-18 19:43:23 +0530

    Completed 01-middleware assignments

--- assignments-master/week-3/01-middlewares/02-ratelimitter.js
+++ assignments-master/week-3/01-middlewares/02-ratelimitter.js
@@ -14,7 +14,22 @@ const app = express();
 let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
-}, 1000)
+}, 1000);
+
+app.use(function(req, res, next){
+  const userId = req.headers["user-id"];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] += 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("No entry");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+});
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
