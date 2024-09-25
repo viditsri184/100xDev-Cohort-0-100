563e3df835eb54e978fd41bf2022800b89b55818
viditsri184 <viditsrivastava760@gmail.com>
2024-09-25 21:58:13 +0530

    added navbar

---
+++ Week 5/week 5.2/frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export function Navbar(){
+    return <div>
+        <nav id="navbar">
+        <div className="logo">
+            <img src="../src/logo.png" alt=""/>
+            <div className="title">TODO APP</div>
+        </div>
+        <div className="options">
+            <ul>
+                <li>About Us</li>
+                <li>Blog</li>
+                <li>SignUp</li>
+            </ul>
+        </div>
+    </nav>
+    </div>
+}
