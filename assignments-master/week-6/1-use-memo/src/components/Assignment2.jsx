bba0fd6d2c71ff9e7a3c4ce843316a6027359fbd
viditsri184 <viditsrivastava760@gmail.com>
2024-09-28 14:37:23 +0530

    completed week6 1-use-memo assignments

--- assignments-master/week-6/1-use-memo/src/components/Assignment2.jsx
+++ assignments-master/week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,33 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 // In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
 // You will learn something new here, specifically how you have to pass more than one value in the dependency array
 
-const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
-const TOTAL_LINES = 1000;
-const ALL_WORDS = [];
-for (let i = 0; i < TOTAL_LINES; i++) {
-    let sentence = "";
-    for (let j = 0; j < words.length; j++) {
-        sentence += (words[Math.floor(words.length * Math.random())])
-        sentence += " "
+const WORDS = ["Hello", "my", "name", "is", "popeyee", "the sailor", "man"];
+const total_lines = 1000;
+const ALL_SENTENCES = [];
+for(let i = 0 ; i < total_lines ; i++){
+    let sentences = "";
+    for(let j =  0 ; j < WORDS.length ; j++){
+        sentences += WORDS[Math.floor(Math.random() * WORDS.length)];
+        sentences += " ";
     }
-    ALL_WORDS.push(sentence);
+    ALL_SENTENCES.push(sentences);
 }
 
-export function Assignment2() {
-    const [sentences, setSentences] = useState(ALL_WORDS);
+export function Assignment2(){
+    const [sentence, setSentences] = useState(ALL_SENTENCES);
     const [filter, setFilter] = useState("");
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
+    let filteredSentences = useMemo(() => {return sentence.filter(x => x.includes(filter))}, [sentence, filter]);
 
     return <div>
-        <input type="text" onChange={(e) => {
-            setFilter(e.target.value)
-        }}></input>
-        {filteredSentences.map(word => <div>
-            {word}    
-        </div>)}
+        <input type="text" onChange={(e) => setFilter(e.target.value)}/>
+        {filteredSentences.map(word => <div>{word}</div>)}
     </div>
 }
