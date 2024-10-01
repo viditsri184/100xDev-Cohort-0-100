83885483f412bdf8b7e559508f187663dda34bdc
viditsri184 <viditsrivastava760@gmail.com>
2024-10-01 16:04:02 +0530

    implemented recoil easy concepts

---
+++ Week 7/week 7.3/Recoil_easy_bits/src/App.jsx
@@ -0,0 +1,53 @@
+import { netWorkAtom, notificationsAtom, messagingAtom, jobsAtom, totalNotificationSelector } from './atoms';
+import './App.css';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+
+function App() {
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+}
+
+function MainApp() {
+
+  const networkNotificationCount = useRecoilValue(netWorkAtom);
+  const notificationCount = useRecoilValue(notificationsAtom);
+  const messageNotificationCount = useRecoilValue(messagingAtom);
+  const jobNotificationCount = useRecoilValue(jobsAtom);
+
+  // The other way is to use Selectors in recoil
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+
+  // one way to do this useMemo
+  // const totalNotificationCount = useMemo(() =>{
+  //   return (networkNotificationCount + notificationCount + messageNotificationCount + jobNotificationCount)
+  // }, [networkNotificationCount, notificationCount, messageNotificationCount, jobNotificationCount]);
+
+  return (
+    <div style={styles.container}>
+      <button style={styles.buttons}>Home</button>
+      <button style={styles.buttons}>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button style={styles.buttons}>Jobs ({jobNotificationCount >= 100 ? "99+" : jobNotificationCount})</button>
+      <button style={styles.buttons}>Messaging ({messageNotificationCount >= 100 ? "99+" : messageNotificationCount})</button>
+      <button style={styles.buttons}>Notifications ({notificationCount >= 100 ? "99+" : notificationCount})</button>
+      <button style={styles.buttons}>Me ({totalNotificationCount})</button>
+    </div>
+
+  )
+}
+
+export default App
+
+const styles = {
+  container: {
+    padding: 5,
+    background: "#fff",
+  },
+  buttons: {
+    margin: 10,
+    padding: 10,
+    border: "1px solid black",
+    borderRadius: "20px"
+  }
+}
---
+++ Week 7/week 7.3/Recoil_easy_bits/src/atoms.js
@@ -0,0 +1,35 @@
+import {atom, selector} from 'recoil';
+
+export const netWorkAtom = atom({
+    key:"networkAtom",
+    default:102
+});
+
+export const jobsAtom = atom({
+    key:"jobsAtom",
+    default:0
+});
+
+export const notificationsAtom = atom({
+    key: "notificationsAtom",
+    default:12
+});
+
+
+export const messagingAtom = atom({
+    key:"messagingAtom",
+    default:5
+});
+
+
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get: ({get}) => {
+        const networkNotificationCount = get(netWorkAtom);
+        const jobNotificationCount = get(jobsAtom);
+        const notificationCount = get(notificationsAtom);
+        const messageNotificationCount = get(messagingAtom);
+
+        return networkNotificationCount + jobNotificationCount + notificationCount + messageNotificationCount;
+    }
+})
