1a3fadfc354f4cd1450d2e2156d1b99597604b97
viditsri184 <viditsrivastava760@gmail.com>
2024-10-05 19:50:18 +0530

    added routes to the pages

--- Week 8/week 8.2/Paytm_project/frontend/src/App.jsx
+++ Week 8/week 8.2/Paytm_project/frontend/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Suspense } from 'react'
+import React from 'react';
+const SignUp = React.lazy(() => import("./pages/SignUp"));
+const SignIn = React.lazy(() => import("./pages/SignIn"));
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const SendMoney = React.lazy(() => import("./pages/SendMoney"));
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/send' element={<SendMoney/>}/>
+        </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
   )
 }
