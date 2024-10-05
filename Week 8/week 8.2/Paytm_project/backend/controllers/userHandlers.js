d27a1646d32009afac7b674df0e7194e765a4fed
viditsri184 <viditsrivastava760@gmail.com>
2024-10-05 15:23:00 +0530

    fixed bugs in userhandlers

--- Week 8/week 8.2/Paytm_project/backend/controllers/userHandlers.js
+++ Week 8/week 8.2/Paytm_project/backend/controllers/userHandlers.js
@@ -5,7 +5,8 @@ const jwt = require("jsonwebtoken");
 const {JWT_SECRET} = require("../config");
 const { signUpBody, signInBody, updateBody } = require("../types");
 
-async function signUpHandler() {
+// ------ Signup Handler ------
+async function signUpHandler(req, res) {
     const {success} = signUpBody.safeParse(req.body);
     if(!success){
         return res.status(411).json({message : "Email already taken/incorrect inputs"});
@@ -17,14 +18,14 @@ async function signUpHandler() {
         return res.status(411).json({msg : "User already exists"});
         
     }
-    const newUser = {
+    const newUser = new User({
         username : req.body.username,
         firstName: req.body.firstName,
         lastName: req.body.lastName
-    }
+    })
     const hashedPassword = await newUser.createHash(req.body.password);
     newUser.password_hash = hashedPassword;
-    const user = await User.create(newUser);
+    const user = await newUser.save();
     const userId = user._id;
 
     // ----- Create new account ------
@@ -34,11 +35,12 @@ async function signUpHandler() {
     });
     // ----------------------------------
 
-    const token = jwt.sign(userId, JWT_SECRET);
+    const token = jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: '1h' });
     return res.status(201).json({message: "User created successfully", token: token});
 }
 
-async function signInHandler(){
+// ------ Signin Handler ------
+async function signInHandler(req, res){
     const {success} = signInBody.safeParse(req.body);
 
     if(!success){
@@ -50,19 +52,31 @@ async function signInHandler(){
         return res.status(411).json({message: "User doesn't exist"});
     }
     else{
-        const token = jwt.sign(existingUser._id, JWT_SECRET);
+        const validPassword = await existingUser.validatePassword(req.body.password);
+        if (!validPassword) {
+            return res.status(401).json({ message: "Incorrect password" });
+        }
+
+        const token = jwt.sign({ id: existingUser._id }, JWT_SECRET, { expiresIn: '1h' });
         return res.status(200).json({token: token});
     }
 }
 
-async function userInfoUpdate(){
+// ------ Update User Info ------
+async function userInfoUpdate(req, res){
     const {success} = updateBody.safeParse(req.body);
 
     if(!success){
         return res.status(411).json({message: "Invalid Input"});
     }
     const userId = req.userId;
-    const hashedPassword = await newUser.createHash(req.body.password);
+    const existingUser = await User.findOne({_id: userId});
+    // Check if the user exists
+    if (!existingUser) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    const hashedPassword = await existingUser.createHash(req.body.password);
     await User.findOneAndUpdate({_id : userId}, {
         $set: {password_hash: hashedPassword, firstName: req.body.firstName, lastName: req.body.lastName}
     });
@@ -70,9 +84,17 @@ async function userInfoUpdate(){
     return res.status(200).json({message: "Updated successfully"});
 }
 
-async function getUser(){
+
+// ------ Get Users ------
+async function getUser(req, res){
     const filter = req.query.filter || "";
-    const users = await User.find({$or: [{firstName: {"$regex": filter}}, {lastName: {"$regex": filter}}]});
+    // Use 'i' flag to make the regex case-insensitive
+    const users = await User.find({
+        $or: [
+            { firstName: { "$regex": filter, "$options": "i" } },
+            { lastName: { "$regex": filter, "$options": "i" } }
+        ]
+    });
 
     return res.status(200).json({
         user:users.map(user => ({
