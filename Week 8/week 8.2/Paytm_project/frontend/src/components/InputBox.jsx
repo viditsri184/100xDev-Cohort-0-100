65a9c46a81bd058e6dcce361d608a6f763bb5111
viditsri184 <viditsrivastava760@gmail.com>
2024-10-06 20:19:30 +0530

    added backend connection to the component

--- Week 8/week 8.2/Paytm_project/frontend/src/components/InputBox.jsx
+++ Week 8/week 8.2/Paytm_project/frontend/src/components/InputBox.jsx
@@ -1,8 +1,8 @@
-export function InputBox({ title, placeholder }) {
+export function InputBox({ title, placeholder, onChange }) {
     return (
         <div>
             <label htmlFor="first_name" className="block mb-2 text-sm text-left font-semibold mt-3 pb-1">{title}</label>
-            <input required type="text" placeholder={placeholder} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5" />
+            <input required onChange={onChange} type="text" placeholder={placeholder} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5" />
         </div>
     )
 }
