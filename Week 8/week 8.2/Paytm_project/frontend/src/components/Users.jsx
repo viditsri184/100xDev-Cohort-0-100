d0db8d941df6d870240f205bbdbff63f242e3ee5
viditsri184 <viditsrivastava760@gmail.com>
2024-10-06 20:20:07 +0530

    connected backend to the ui

--- Week 8/week 8.2/Paytm_project/frontend/src/components/Users.jsx
+++ Week 8/week 8.2/Paytm_project/frontend/src/components/Users.jsx
@@ -1,17 +1,46 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react"
+import { User } from "./User";
+
+// custom hook to perform debouncing
+function useDebounce(value, delay) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        // Cleanup function to clear the timeout if the value changes before the delay
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+}
+
+
 export function Users() {
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
+    const debouncedFilter = useDebounce(filter, 500); // Adjust delay as necessary
+
+    useEffect(() => {
+        if (debouncedFilter || debouncedFilter === "") { // Only fetch if there's a debounced filter value
+            axios.get(`http://localhost:3000/api/v1/user/bulk/?filter=${debouncedFilter}`)
+                .then((response) => {
+                    setUsers(response.data.user);
+                });
+        }
+    }, [debouncedFilter]);
+
+
     return <div className="mt-5">
         <div className="font-bold text-lg">Users</div>
-        <input type="text" placeholder="Search users" className="block border w-full h-10 mt-3 mb-3 rounded-lg outline-none placeholder:pl-1 pl-5" />
-        <div className="flex justify-between mt-2">
-            <div className="flex">
-                <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                    <div className="flex flex-col justify-center h-full text-xl">U</div>
-                </div>
-                <div className="flex flex-col justify-center h-full mr-4 leading-[50px] text-lg font-semibold">Vidit Srivastava</div>
-            </div>
-            <div className="flex flex-col justify-center">
-                <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 ">Send Money</button>
-            </div>
+        <input type="text" onInput={(e) => { setFilter(e.target.value)}} placeholder="Search users" className="block border w-full h-10 mt-3 mb-3 rounded-lg outline-none placeholder:pl-1 pl-5" />
+        <div className="mt-2">
+        {users.length === 0 ? <div className="text-xl font-semibold text-gray-500 mt-2 ml-2">No Users found!!</div> : users.map((user) => {return <User key={user.id} user={user}/>})}
         </div>
     </div>
 }
