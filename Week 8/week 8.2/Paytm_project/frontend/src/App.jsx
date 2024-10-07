1e356c4477f697f7d5f9dc91c269c5919c5f35f5
viditsri184 <viditsrivastava760@gmail.com>
2024-10-07 11:14:14 +0530

    updated app.jsx

--- Week 8/week 8.2/Paytm_project/frontend/src/App.jsx
+++ Week 8/week 8.2/Paytm_project/frontend/src/App.jsx
@@ -5,7 +5,7 @@ const SignUp = React.lazy(() => import("./pages/SignUp"));
 const SignIn = React.lazy(() => import("./pages/SignIn"));
 const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 const SendMoney = React.lazy(() => import("./pages/SendMoney"));
-
+const Entry = React.lazy(() => import("./pages/Entry"));
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
         <Routes>
+          <Route path='/' element={<Entry/>}/>
           <Route path='/signup' element={<SignUp/>}/>
           <Route path='/signin' element={<SignIn/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
