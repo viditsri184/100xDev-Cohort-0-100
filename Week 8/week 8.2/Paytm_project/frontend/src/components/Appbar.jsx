1023dccc0301c86464d8e71936e3d3749b7e4fe3
viditsri184 <viditsrivastava760@gmail.com>
2024-10-07 11:15:04 +0530

    added logout button in the appbar component

--- Week 8/week 8.2/Paytm_project/frontend/src/components/Appbar.jsx
+++ Week 8/week 8.2/Paytm_project/frontend/src/components/Appbar.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom"
+
 export function Appbar({user}) {
+    const navigate = useNavigate();
+
     return <div className="flex justify-between h-14 shadow">
         <div className="flex flex-col justify-center h-full ml-4 font-extrabold text-xl">Paytm App</div>
         <div className="flex">
             <div className="flex flex-col justify-center h-full mr-4">Hello {user}</div>
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">U</div>
+            </div>
+            <div className="flex flex-col justify-center">
+                <button onClick={() => {
+                    localStorage.removeItem("token");
+                    navigate("/");
+                }} className="cursor-pointer h-[40px] border rounded-lg w-[70px] flex flex-col justify-center items-center bg-black text-white">Log Out</button>
             </div>
         </div>
     </div>
